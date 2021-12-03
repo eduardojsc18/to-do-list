cc4ac5c426f7fbea0488535a725697880e98de9b
Eduardo José <81324243+eduardojsc18@users.noreply.github.com>
2021-12-03 16:59:11 -0300

    [UPDATE] Index
    - Created desgin index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,31 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+
+        fontFamily: {
+
+            'roboto': ['Roboto', 'sans-serif'],
+            'roboto-condensed': ['"Roboto Condensed"', 'sans-serif']
+
+        },
+
+        extend: {
+
+            zIndex: {
+                '-1': '-1'
+            }
+
+        },
+
+    },
+    variants: {
+        extend: {
+
+            borderColor: ['active'],
+            textColor: ['group-focus']
+
+        },
+    },
+    plugins: [],
 }
